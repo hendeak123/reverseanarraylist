b448752da3ae5e584d8badf537db9c1c4bbb25f9
Alyssa K. Henderson <alyssa.k.henderson@svu.edu>
2020-10-21 14:00:13 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<String>();
+        populateArray(animals);
+        reverseArray(animals);
+    }
+
+    private static void populateArray(ArrayList<String> animals) {
+        try {
+            animals.add("Cat");
+            animals.add("Dog");
+            animals.add("Elephant");
+            animals.add("Giraffe");
+            animals.add("Squirrel");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println(animals);
+        }
+    }
+
+    private static void reverseArray(ArrayList<String> animals) {
+        ArrayList<String> reverseAnimals = new ArrayList<String>();
+        try {
+            for (int count = animals.size(); count < animals.size(); count--){
+                reverseAnimals.add(animals.get(count));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println(reverseAnimals);
+        }
+    }
+}
